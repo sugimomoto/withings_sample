c4875580fe91afded82d65210cf8d34372b0e07d
sugimomoto <sugimomoto@gmail.com>
2021-12-20 07:12:09 +0900

    Added scope pattern

--- src/main/java/sugimomoto/Scope.java
+++ src/main/java/sugimomoto/Scope.java
@@ -1,5 +1,16 @@
 package sugimomoto;
 
 public enum Scope {
+    USER_ACTIVITY("user.activity"),
+    USER_METRICS("user.metrics");
 
+    private final String value;
+
+    private Scope(String value){
+        this.value = value;
+    }
+
+    public String getValue(){
+        return value;
+    }
 }
