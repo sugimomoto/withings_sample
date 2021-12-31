8c6974410c302d041add986017a3911c8182fa7c
sugimomoto <sugimomoto@gmail.com>
2021-12-31 09:45:11 +0900

    Added API Client first commit

--- src/main/java/sugimomoto/WithingsAPIClient.java
+++ src/main/java/sugimomoto/WithingsAPIClient.java
@@ -4,5 +4,9 @@ public class WithingsAPIClient {
 
     public WithingsAPIClient(AccessTokenResponse token) {
     }
+
+    public ActivityBase getActivities(ActivitiesQueryParameters param) {
+        return null;
+    }
     
 }
