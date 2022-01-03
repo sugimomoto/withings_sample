c5b499386471d3438c41ed6b687213354a44bc2c
sugimomoto <sugimomoto@gmail.com>
2022-01-04 07:45:08 +0900

    Added MeasCategory and MeasType of Enum

--- src/main/java/sugimomoto/MeasCategory.java
+++ src/main/java/sugimomoto/MeasCategory.java
@@ -1,6 +1,16 @@
 package sugimomoto;
 
 public enum MeasCategory {
-    RealMeasures
-    
+    RealMeasures(1),
+    UserObjectives(2);
+
+    private final int value;
+
+    private MeasCategory(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
 }
--- src/main/java/sugimomoto/MeasType.java
+++ src/main/java/sugimomoto/MeasType.java
@@ -1,6 +1,36 @@
 package sugimomoto;
 
 public enum MeasType {
-    Weight, Height
-    
+    Weight(1),
+    Height(4),
+    FatFreeMass(5),
+    FatRatio(6),
+    FatMassWeight(8),
+    DiastolicBloodPressure(9),
+    SystolicBloodPressure (10),
+    HeartPulse(11),
+    Temperature(12),
+    SP02(54),
+    BodyTemperature(71),
+    SkinTemperature(73),
+    MuscleMass(76),
+    Hydration(77),
+    BoneMass(88),
+    PulseWaveVelocity(91),
+    VO2MaxIsAnumericalMeasurementOfYourBodysAbilityToConsumeOxygen(123),
+    QRSIntervalDurationBasedOnECGSignal(135),
+    PRIntervalDurationBasedOnECGSignal(136),
+    QTIntervalDurationBasedOnECGSignal(137),
+    CorrectedQTIntervalDurationBasedOnECGSignal(138),
+    AtrialFibrillationResultFromPPG(139);
+
+    private final int value;
+
+    private MeasType(int value){
+        this.value = value;
+    }
+
+    public int getValue(){
+        return value;
+    }
 }
