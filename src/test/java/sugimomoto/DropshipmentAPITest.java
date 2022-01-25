ac63358862fea43879b1f4b7c287659ca86fa50e
sugimomoto <sugimomoto@gmail.com>
2022-01-26 08:00:08 +0900

    Fixed DropshipmentAPITest

--- src/test/java/sugimomoto/DropshipmentAPITest.java
+++ src/test/java/sugimomoto/DropshipmentAPITest.java
@@ -3,6 +3,7 @@ package sugimomoto;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
@@ -51,18 +52,137 @@ public class DropshipmentAPITest extends APIClientTestSettup {
     }
 
     @Test
+    public void DropshipmentCreateUserOrderQueryParametersTest(){
+
+        DropshipmentCreateUserOrderQueryParameters param = new DropshipmentCreateUserOrderQueryParameters();
+        
+        param.setClientId("test_client_id");
+        param.setNonce("test_nonce");
+        param.setMailingpref(false);
+        param.setBirthdate(1563746400);
+
+
+        param.setOrder(getSampleOrders());
+        param.setCustomerId("test_customer_id");
+        param.setTestMode(1);
+
+        FormBody formBody = param.getQueryParameters();
+
+        assertEquals(7, formBody.size());
+        
+        assertEquals("action", formBody.name(0));
+        assertEquals("createorder", formBody.value(0));
+        
+        assertEquals("client_id", formBody.name(1));
+        assertEquals("test_client_id", formBody.value(1));
+        
+        assertEquals("nonce", formBody.name(2));
+        assertEquals("test_nonce", formBody.value(2));
+
+        assertEquals("order", formBody.name(3));
+        assertEquals("[{\"address\":{\"name\":\"John Wayne\",\"email\":\"john.wayne@farwest.com\",\"address1\":\"1st Avenue\",\"address2\":\"Apartment 12\",\"city\":\"New-York\",\"zip\":\"10001\",\"state\":\"NY\",\"country\":\"US\"},\"customer_ref_id\":\"IMAGINARY_CUSTOMER_REF_1\",\"products\":[{\"quantity\":1,\"ean\":\"WI220002EAN\"},{\"quantity\":3,\"ean\":\"WI330002EAN\"}]},{\"address\":{\"name\":\"Calamity Jane\",\"email\":\"calamity.jane@farwest.com\",\"address1\":\"SW 8th St\",\"address2\":\"Block 1\",\"city\":\"Miami\",\"zip\":\"33135\",\"state\":\"FL\",\"country\":\"US\"},\"customer_ref_id\":\"IMAGINARY_CUSTOMER_REF_2\",\"products\":[{\"quantity\":2,\"ean\":\"WI220002EAN\"},{\"quantity\":1,\"ean\":\"WI440002EAN\"},{\"quantity\":1,\"ean\":\"WI550002EAN\"}]}]", formBody.value(3));
+
+        assertEquals("customerid", formBody.name(4));
+        assertEquals("test_customer_id", formBody.value(4));
+
+        assertEquals("testmode", formBody.name(5));
+        assertEquals("1", formBody.value(5));
+
+        assertEquals("signature", formBody.name(6));
+        assertEquals("sample", formBody.value(6));
+    }
+
+    @Test
     public void DropshipmentDeleteQueryParametersTest(){
-        throw new AssertionFailedError();
+        DropshipmentDeleteQueryParameters param = new DropshipmentDeleteQueryParameters();
+        param.setClientId("test_client_id");
+        param.setNonce("test_nonce");
+        param.setOrderId("test_order_id");
+
+        FormBody formBody = param.getQueryParameters();
+        assertEquals(5, formBody.size());
+        
+        assertEquals("action", formBody.name(0));
+        assertEquals("delete", formBody.value(0));
+        
+        assertEquals("client_id", formBody.name(1));
+        assertEquals("test_client_id", formBody.value(1));
+        
+        assertEquals("nonce", formBody.name(2));
+        assertEquals("test_nonce", formBody.value(2));
+
+        assertEquals("order_id", formBody.name(3));
+        assertEquals("test_order_id", formBody.value(3));
+
+        assertEquals("signature", formBody.name(4));
+        assertEquals("sample", formBody.value(4));
     }
 
     @Test
     public void DropshipmentGetorderstatusQueryParametersTest(){
-        throw new AssertionFailedError();
+
+        DropshipmentGetorderstatusQueryParameters param = new DropshipmentGetorderstatusQueryParameters();
+        param.setClientId("test_client_id");
+        param.setNonce("test_nonce");
+        param.setOrderIds(Arrays.asList("test_order_id_1","test_order_id_2"));
+        param.setCustomerRefIds(Arrays.asList("test_customer_ref_id_1","test_customer_ref_id_2"));
+        param.setCustomerId("test_customer_id");
+
+        FormBody formBody = param.getQueryParameters();
+        assertEquals(7, formBody.size());
+        
+        assertEquals("action", formBody.name(0));
+        assertEquals("getorderstatus", formBody.value(0));
+        
+        assertEquals("client_id", formBody.name(1));
+        assertEquals("test_client_id", formBody.value(1));
+        
+        assertEquals("nonce", formBody.name(2));
+        assertEquals("test_nonce", formBody.value(2));
+
+        assertEquals("order_ids", formBody.name(3));
+        assertEquals("[\"test_order_id_1\",\"test_order_id_2\"]", formBody.value(3));
+
+        assertEquals("customer_ref_ids", formBody.name(4));
+        assertEquals("[\"test_customer_ref_id_1\",\"test_customer_ref_id_2\"]", formBody.value(4));
+
+        assertEquals("customerid", formBody.name(5));
+        assertEquals("test_customer_id", formBody.value(5));
+
+        assertEquals("signature", formBody.name(6));
+        assertEquals("sample", formBody.value(6));
     }
 
     @Test
-    public void DropshipmentUpdateQueryParametersTest(){
-        throw new AssertionFailedError();
+    public void DropshipmentUpdateQueryParametersTest() throws JsonProcessingException{
+        DropshipmentUpdateQueryParameters param = new DropshipmentUpdateQueryParameters();
+        
+        param.setClientId("test_client_id");
+        param.setNonce("test_nonce");
+        param.setOrderId("test_order_id");
+        param.setOrder(getSampleOrders());
+
+        FormBody formBody = param.getQueryParameters();
+
+        assertEquals(6, formBody.size());
+        
+        assertEquals("action", formBody.name(0));
+        assertEquals("update", formBody.value(0));
+        
+        assertEquals("client_id", formBody.name(1));
+        assertEquals("test_client_id", formBody.value(1));
+        
+        assertEquals("nonce", formBody.name(2));
+        assertEquals("test_nonce", formBody.value(2));
+
+        assertEquals("order_id", formBody.name(3));
+        assertEquals("test_order_id", formBody.value(3));
+
+        assertEquals("order", formBody.name(4));
+        assertEquals("[{\"address\":{\"name\":\"John Wayne\",\"email\":\"john.wayne@farwest.com\",\"address1\":\"1st Avenue\",\"address2\":\"Apartment 12\",\"city\":\"New-York\",\"zip\":\"10001\",\"state\":\"NY\",\"country\":\"US\"},\"customer_ref_id\":\"IMAGINARY_CUSTOMER_REF_1\",\"products\":[{\"quantity\":1,\"ean\":\"WI220002EAN\"},{\"quantity\":3,\"ean\":\"WI330002EAN\"}]},{\"address\":{\"name\":\"Calamity Jane\",\"email\":\"calamity.jane@farwest.com\",\"address1\":\"SW 8th St\",\"address2\":\"Block 1\",\"city\":\"Miami\",\"zip\":\"33135\",\"state\":\"FL\",\"country\":\"US\"},\"customer_ref_id\":\"IMAGINARY_CUSTOMER_REF_2\",\"products\":[{\"quantity\":2,\"ean\":\"WI220002EAN\"},{\"quantity\":1,\"ean\":\"WI440002EAN\"},{\"quantity\":1,\"ean\":\"WI550002EAN\"}]}]", formBody.value(4));
+
+        assertEquals("signature", formBody.name(5));
+        assertEquals("sample", formBody.value(5));
     }
 
     @Test
