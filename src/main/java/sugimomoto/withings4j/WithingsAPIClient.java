d6c766370a9bc6ec4aa266dfc480c9e32ba85798
sugimomoto <sugimomoto@gmail.com>
2022-02-05 10:01:32 +0900

    Added all API Client methods of Withings

--- src/main/java/sugimomoto/withings4j/WithingsAPIClient.java
+++ src/main/java/sugimomoto/withings4j/WithingsAPIClient.java
@@ -1,6 +1,7 @@
 package sugimomoto.withings4j;
 
 import java.io.IOException;
+import java.util.function.ObjDoubleConsumer;
 
 import okhttp3.*;
 import sugimomoto.withings4j.model.*;
@@ -27,14 +28,6 @@ public class WithingsAPIClient extends APIClient {
         return this.endpointUrl + resource.getValue();
     }
 
-    public ActivityBase getActivities(MeasurevGetactivityQueryParameters param) throws IOException, WithingsAPIException {
-        return (ActivityBase)getAPIResponse(getUrl(Resource.V2_MEASURE), param, ActivityBase.class);
-    }
-
-    public MeasBase getMeasures(MeasureGetmeasQueryParameters param) throws IOException, WithingsAPIException {
-        return (MeasBase)getAPIResponse(getUrl(Resource.V1_MEASURE), param, MeasBase.class);
-    }
-
     public OrderBase dropshipmentCreateorder(DropshipmentCreateOrderQueryParameters param) throws WithingsAPIException, IOException {
         return (OrderBase)getAPIResponse(getUrl(Resource.V2_DROPSHIPMENT), param, OrderBase.class);
     }
@@ -55,8 +48,85 @@ public class WithingsAPIClient extends APIClient {
         return (OrderBase)getAPIResponse(getUrl(Resource.V2_DROPSHIPMENT), param, OrderBase.class);
     }
 
+    public HeartBase heartGet(HeartGetQueryParameters param) throws WithingsAPIException, IOException{
+        return (HeartBase)getAPIResponse(getUrl(Resource.V2_HEART), param, HeartBase.class);
+    }
 
+    public HeartListBase heartList(HeartListQueryParameters param) throws WithingsAPIException, IOException{
+        return (HeartListBase)getAPIResponse(getUrl(Resource.V2_HEART), param, HeartListBase.class);
+    }
 
+    public ActivityBase MeasureGetactivity(MeasurevGetactivityQueryParameters param) throws IOException, WithingsAPIException {
+        return (ActivityBase)getAPIResponse(getUrl(Resource.V2_MEASURE), param, ActivityBase.class);
+    }
+
+    public MeasBase measureGetmeas(MeasureGetmeasQueryParameters param) throws IOException, WithingsAPIException {
+        return (MeasBase)getAPIResponse(getUrl(Resource.V1_MEASURE), param, MeasBase.class);
+    }
+
+    public IntradayActivityBase measureGetintradayactivity(MeasureGetintradayactivityQueryParameters param) throws WithingsAPIException, IOException{
+        return (IntradayActivityBase)getAPIResponse(getUrl(Resource.V2_MEASURE), param, IntradayActivityBase.class);
+    }
+
+    public WorkoutsBase measureGetworkouts(MeasureGetworkoutsQueryParameters param) throws WithingsAPIException, IOException{
+        return (WorkoutsBase)getAPIResponse(getUrl(Resource.V2_MEASURE), param, WorkoutsBase.class);
+    }
+
+    public NotifyBase notifyGet(NotifyGetQueryParameters param) throws WithingsAPIException, IOException{
+        return (NotifyBase)getAPIResponse(getUrl(Resource.V1_NOTIFY), param, NotifyBase.class);
+    }
+
+    public NotifyListBase notifyList(NotifyListQueryParameters param) throws WithingsAPIException, IOException{
+        return (NotifyListBase)getAPIResponse(getUrl(Resource.V1_NOTIFY), param, NotifyListBase.class);
+    }
+
+    public ResponseBase notifyRevoke(NotifyRevokeQueryParameters param) throws WithingsAPIException, IOException{
+        return (ResponseBase)getAPIResponse(getUrl(Resource.V1_NOTIFY), param, ResponseBase.class);
+    }
+
+    public ResponseBase notifySubscribe(NotifySubscribeQueryParameters param) throws WithingsAPIException, IOException{
+        return (ResponseBase)getAPIResponse(getUrl(Resource.V1_NOTIFY), param, ResponseBase.class);
+    }
+
+    public ResponseBase notifyUpdate(NotifyUpdateQueryParameters param) throws WithingsAPIException, IOException{
+        return (ResponseBase)getAPIResponse(getUrl(Resource.V1_NOTIFY), param, ResponseBase.class);
+    }
+
+    public SignatureBase signatureGetnonce(SignatureGetnonceQueryParameters param) throws WithingsAPIException, IOException{
+        return (SignatureBase)getAPIResponse(getUrl(Resource.V2_SIGNATURE), param, SignatureBase.class);
+    }
+
+    public SleepBase sleepGet(SleepGetQueryParameters param) throws WithingsAPIException, IOException{
+        return (SleepBase)getAPIResponse(getUrl(Resource.V2_SLEEP), param, SleepBase.class);
+    }
+
+    public SleepSummaryBase sleepGetsummary(SleepGetsummaryQueryParameters param) throws WithingsAPIException, IOException{
+        return (SleepSummaryBase)getAPIResponse(getUrl(Resource.V2_SLEEP), param, SleepSummaryBase.class);
+    }
+
+    public UserDeviceBase userActivate(UserActivateQueryParameters param) throws WithingsAPIException, IOException{
+        return (UserDeviceBase)getAPIResponse(getUrl(Resource.V2_USER), param, UserDeviceBase.class);
+    }
+
+    public UserBase userGet(UserGetQueryParameters param) throws WithingsAPIException, IOException{
+        return (UserBase)getAPIResponse(getUrl(Resource.V2_USER), param, UserBase.class);
+    }
+
+    public DeviceBase userGetDevice(UserGetdeviceQueryParameters param) throws WithingsAPIException, IOException{
+        return (DeviceBase)getAPIResponse(getUrl(Resource.V2_USER), param, DeviceBase.class);
+    }
+
+    public GoalsBase userGetGoals(UserGetgoalsQueryParameters param) throws WithingsAPIException, IOException{
+        return (GoalsBase)getAPIResponse(getUrl(Resource.V2_USER), param, GoalsBase.class);
+    }
+
+    public DeviceBase userLink(UserLinkQueryParameters param) throws WithingsAPIException, IOException{
+        return (DeviceBase)getAPIResponse(getUrl(Resource.V2_USER), param, DeviceBase.class);
+    }
+
+    public ResponseBase userUnlink(UserUnlinkQueryParameters param) throws WithingsAPIException, IOException{
+        return (ResponseBase)getAPIResponse(getUrl(Resource.V2_USER), param, ResponseBase.class);
+    }
 
     public void setToken(AccessToken token){
         this.token = token;
